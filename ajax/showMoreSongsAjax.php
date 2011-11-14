23944ac79e719c0cd7cf833a343f802d6c8efa4d
Calvin Hawkes <calvinhawkes@gmail.com>
2011-11-14 10:53:08 -0500

    show more songs works

---
+++ ajax/showMoreSongsAjax.php
@@ -0,0 +1,46 @@
+<?php
+include('../connection.php');
+include ("../classes/Song.php");
+
+if($_POST)
+{	
+	$where = $_POST['where'];
+	$songsPerPage = $_POST['songsPerPage'];
+	$upperLimit = $_POST['upperLimit'];
+	
+	//updating limits
+	$i = $upperLimit + 1;
+	$lowerLimit = $upperLimit;
+	$upperLimit = $upperLimit + $songsPerPage;
+	
+	if(strpos($where, 'New') != false) //newest was selected
+	{
+		$qry = mysql_query("SELECT * FROM  `songs` 
+						WHERE $where
+						ORDER BY uploaded_on DESC
+						LIMIT $lowerLimit , $upperLimit
+						");
+	}
+	else
+	{
+		$qry = mysql_query("SELECT * FROM  `songs` 
+						WHERE $where
+						ORDER BY score DESC
+						LIMIT $lowerLimit , $upperLimit
+						");
+	}					
+	if (!$qry)
+		die("FAIL: " . mysql_error());
+
+	
+	while($row = mysql_fetch_array($qry))
+	{
+		$song = new Song($row, $i);
+		echo '<tr class="song" id="'.$i.'">';
+			$song->showMin();
+		echo '</tr>';
+		$i ++;
+	}
+}
+else { die("FAIL: POST not set in maxSongAjax.php"); }
+?>
