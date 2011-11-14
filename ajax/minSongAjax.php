8c5e6d026bc260800b79b236b0ca742dcaf516e0
Calvin Hawkes <calvinhawkes@gmail.com>
2011-11-14 11:26:29 -0500

    NOW show more works

---
+++ ajax/minSongAjax.php
@@ -0,0 +1,23 @@
+<?php
+include('../connection.php');
+include ("../classes/Song.php");
+
+if($_POST)
+{	
+	$title = $_POST['title'];
+	$artist = $_POST['artist'];
+	$genre = $_POST['genre'];
+	$ytcode = $_POST['ytcode'];
+	$user = $_POST['user'];
+	$ups = $_POST['ups'];
+	$downs = $_POST['downs'];
+	$score = $_POST['score'];
+	$i = $_POST['i'];
+	$id = $_POST['id'];
+	$upload_date = $_POST['upload_date'];
+	
+	$song_max = Song::Song_Info($title, $artist, $genre, $ytcode, $user, $score, $ups, $downs, $id, $upload_date, $i);
+	$song_max->showMin();
+}
+else { die("FAIL: POST not set in maxSongAjax.php"); }
+?>
