f631b429068c5823a6c178aa4b9512319e94c755
Calvin Hawkes <calvinhawkes@gmail.com>
2012-02-20 15:53:48 -0500

    users can only vote once on songs, did this by setting a cookie of the youtube value on a vote

--- ajax/voteAjax.php
+++ ajax/voteAjax.php
@@ -10,42 +10,45 @@ if($_POST)
 	$downs = $_POST['downs'];
 	$user = $_POST['user'];
 
-    //calulating new score, ups, downs
-    $toAdd = -1; //what to add to the user's score. 
-    if($result == 'up')
-    {
-        $score ++;
-        $ups ++;
-        $toAdd = 1;
+    //first, let's make sure the user hasn't voted before
+    if(isset($_COOKIE[$ytcode])) { 
+        //user voted before, let's just return the same score
+	echo '<span class="score">' . $score . '</span> [' . $ups . '/' . $downs . '] ';
+    } else {//user hasn't voted before
+
+        //let's set a cookie so the user can't vote again
+        $inTwoMonths = 60 * 60 * 24 * 60 + time(); 
+        setcookie($ytcode, date("G:i - m/d/y"), $inTwoMonths);
+
+        //calulating new score, ups, downs
+        $toAdd = -1; //what to add to the user's score. 
+        if($result == 'up')
+        {
+            $score ++;
+            $ups ++;
+            $toAdd = 1;
+        }
+        else //result == 'down'
+        {
+            $score --;
+            $downs ++;
+        }
+
+        //updating song's score in database
+        $qry = "UPDATE songs SET score='$score' , ups='$ups', downs='$downs' WHERE youtubecode='$ytcode'";
+        $qry = mysql_query($qry);
+        if (!$qry)
+            die("FAIL: " . mysql_error());
+            
+
+        //updating user's score in database
+        $qry = mysql_query('UPDATE users SET points=points + '. $toAdd . ' WHERE user="'. $user .'"');
+        if (!$qry)
+            die("FAIL: " . mysql_error());
+
+        echo '<span class="score">' . $score . '</span> [' . $ups . '/' . $downs . '] ';
     }
-    else //result == 'down'
-    {
-        $score --;
-        $downs ++;
-    }
-
-    //updating song's score in database
-    $qry = "UPDATE songs SET score='$score' , ups='$ups', downs='$downs' WHERE youtubecode='$ytcode'";
-    $qry = mysql_query($qry);
-    if (!$qry)
-        die("FAIL: " . mysql_error());
-	
-	
-	if (isset($_SERVER['HTTP_X_FORWARD_FOR'])) //adding ip for user
-		$ip = $_SERVER['HTTP_X_FORWARD_FOR'];
-	else 
-		$ip = $_SERVER['REMOTE_ADDR'];
-
-	$qry = mysql_query("INSERT INTO ipcheck (ytcode, ip) VALUES ('$ytcode', '$ip')");
-			if (!$qry)
-				die("FAIL: " . mysql_error());
-
-    //updating user's score in database
-    $qry = mysql_query('UPDATE users SET points=points + '. $toAdd . ' WHERE user="'. $user .'"');
-    if (!$qry)
-        die("FAIL: " . mysql_error());
 }
 else { die("FAIL: POST not set in voteUpAjax"); }
 ?>
 
-	<?php echo '<span class="score">' . $score . '</span> [' . $ups . '/' . $downs . '] ';?>
