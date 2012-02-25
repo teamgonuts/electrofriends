779df7eff6fa13372c452e7e9cf33dedc5f6354c
joelshaw5 <joelshaw5@gmail.com>
2012-02-25 14:48:56 -0800

    made entire width of site 50px smaller

--- index.php
+++ index.php
@@ -224,7 +224,7 @@ include ("classes/GenreFilter.php");
 				</ul>
 			</div><!-- end of topList -->
 			<br/>
-			<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2Ft3kno%2F146156005506087&amp;width=300&amp;height=290&amp;colorscheme=light&amp;show_faces=true&amp;border_color=%23cccccc&amp;stream=false&amp;header=true&amp;appId=313109265403858" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:290px;" allowTransparency="true"></iframe>			
+			<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2Ft3kno%2F146156005506087&amp;width=250&amp;height=290&amp;colorscheme=light&amp;show_faces=true&amp;border_color&amp;stream=false&amp;header=true&amp;appId=313109265403858" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:250px; height:290px;" allowTransparency="true"></iframe>		
 			
 			
 		</div><!-- end of sidebar -->
