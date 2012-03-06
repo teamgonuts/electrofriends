923f72b6a6929f6680f621d4771c96dc2db1626e
joelshaw5 <joelshaw5@gmail.com>
2012-03-06 15:01:10 -0800

    added headshots

--- index.php
+++ index.php
@@ -118,7 +118,7 @@ include ("classes/GenreFilter.php");
                                                         <div class="hidden lightbox-div" id="about" style="color:white; width:650px;height:420px; background-color:#000; color #fff; overflow:auto;">
                                                                 <h2>Meet Our Team</h2>
                                                                 
-                                                                <img src="" alt="Calvin Hawkes Picture" />
+                                                                <img src="images/headshots/calvin.jpg" alt="Calvin Hawkes Picture" />
                                                                 <p>Calvin Hawkes [@CalvinHawkes] is an entrepreneur from Los Gatos, CA and lead developer of t3k.no. He 
                                                                    is currently senior at New York University double majoring Computer Science
                                                                    & Economics. As a fan of EDM, Calvin noticed the need for 
@@ -128,7 +128,7 @@ include ("classes/GenreFilter.php");
                                                                    principles. He hopes that by allowing anyone to share songs, t3k.no will stay up to date 
                                                                    with the freshest tracks, creating a database of incredible, free music.</p>
                                                                 
-                                                                <img src="" alt="Joel Shaw Picture">
+                                                                <img src="images/headshots/joel.jpg" alt="Joel Shaw Picture">
                                                                 <p>Joel Shaw [@joelshaw5] is also a Bay Area native, from Cupertino, CA and head designer of t3k.no. He graduated 
                                                                 from Cal Poly San Luis Obispo with a degree in Graphic Communications in March of 2011. 
                                                                 Currently he is working as a graphic designer in the San Francisco Bay Area and San 
